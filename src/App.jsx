256cc3368fcf7625d0937b1a2b61385dd0346512
[Arnaud <[etu48485@henallux.be]>
2023-10-16 16:13:22 +0200

    commit suivant

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Root from './pages/Root';
+import FirstPage from './pages/Firstpage';
+import SecondPage from './pages/SecondPage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
+	const router = createBrowserRouter([
+	{
+		path: '/',
+		element:<Root/>,
+		children:[
+	{
+		path: 'first',
+		element:<FirstPage/>
+	}
+   ]
+  },
+  {
+	path:'second',
+	element:<secondPage/>
+	}
 
-    </>
-  )
-}
+])
+
+return (
+	<>
+		<RouterProvider router={router}/>
+	</>
+   )
+  }
 
 export default App
