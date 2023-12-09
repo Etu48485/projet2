0f4db57d55dc562dd4b2302e8fd9c0105687907e
Arnaud Nicolas <etu48485@henallux.be>
2023-12-09 21:37:58 +0100

    09/12/2023 21H38

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Layout, List, Card, Space } from 'antd';
+import axios from 'axios';
+
+const { Content } = Layout;
+
+const App = () => {
+  const [parkings, setParking] = useState([]);
+
+  useEffect(() => {
+    getParkingData();
+  }, []);
+
+  const getParkingData = () => {
+    axios
+      .get(
+        'https://data.namur.be/api/explore/v2.1/catalog/datasets/namur-mobilite-parking/records?limit=30'
+      )
+      .then((response) => {
+        setParking(response.data.results);
+      })
+      .catch((error) => {
+        console.error('Error fetching parking data:', error);
+      });
+  };
+
+  return (
+    <Layout>
+      <Content style={{ padding: '0 50px' }}>
+        <Space direction="vertical" size={16} style={{ width: '100%' }}>
+          {parkings.map((parking, index) => (
+            <Card
+              key={index}
+              title={parking.plsy_descri}
+              extra={<a href="#">More</a>}
+              style={{ width: '100%' }}
+            >
+              <p>Places: {parking.places}</p>
+              <p>Fonction: {parking.plsy_fonction}</p>
+            </Card>
+          ))}
+        </Space>
+      </Content>
+    </Layout>
+  );
+};
+
+export default App;
+
+/*
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Root from './pages/Root';
@@ -32,3 +82,5 @@ return (
   }
 
 export default App
+
+*/
